77fe35286ade51117e5dc372c9d209b3dc159869
hhhhhxm <huangxingming@kezaihui.com>
2022-11-11 00:18:55 +0800

    修改代码

--- src/main/java/com/kezaihui/faq/controller/ClientController.java
+++ src/main/java/com/kezaihui/faq/controller/ClientController.java
@@ -4,14 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.kezaihui.faq.controller.requestObject.CreateAskRo;
 import com.kezaihui.faq.response.ResultData;
+import com.kezaihui.faq.util.Img2Base64Util;
+import com.kezaihui.faq.vo.PicVo;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -24,25 +28,34 @@ public class ClientController {
 
     private static final String ROB_URL = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=78339ff3-8da5-4af6-98b7-1132abef81cf";
 
+    @Autowired
+    private Img2Base64Util img2Base64Util;
 
-    @PostMapping("/client/create_ask")
+    @PostMapping("/create_ask")
     public ResultData<?> createAsk(@RequestBody CreateAskRo askRo) {
+
+        sendTextToWX(ROB_URL, "销售反馈了一个问题,请及时处理,具体问题如下:" + askRo.getQuestion());
+        sendTextToWX(ROB_URL, askRo.getQuestion());
         if (!CollectionUtils.isEmpty(askRo.getBase64List())) {
             askRo.getBase64List().forEach(base64 -> {
-                sendPicToWX(ROB_URL, base64);
+                base64 = base64.replace("data:image/png;base64,", "");
+                InputStream inputStream = null;
+                inputStream = img2Base64Util.generateImage(base64);
+                //生成base64和md5
+                PicVo imgStr = img2Base64Util.getImgStr(inputStream);
+                sendPicToWX(ROB_URL, imgStr);
             });
         }
-        sendTextToWX(ROB_URL, "问题：" + askRo.getQuestion());
-
         return ResultData.SUCCESS;
     }
 
-    private void sendPicToWX(String url, String base64) {
+    private void sendPicToWX(String url, PicVo imgStr) {
 
         JSONObject queryObj = new JSONObject();
         queryObj.put("msgtype", "image");
         JSONObject contentObj = new JSONObject();
-        contentObj.put("base64", base64);
+        contentObj.put("md5", imgStr.getMd5());
+        contentObj.put("base64", imgStr.getBase64());
         queryObj.put("image", contentObj);
         okhttp3.RequestBody body = okhttp3.RequestBody.create(
                 MediaType.parse("application/json; charset=utf-8"), JSON.toJSONString(queryObj));
